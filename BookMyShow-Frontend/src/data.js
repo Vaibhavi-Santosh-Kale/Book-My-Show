d7c4b70944eb0daca2491e1bb2e940dee1581145
Vaibhavi Santosh Kale <112152930+Vaibhavi-Santosh-Kale@users.noreply.github.com>
2023-07-05 21:37:13 +0530

    Last Commit

---
+++ BookMyShow-Frontend/src/data.js
@@ -0,0 +1,15 @@
+export let moviesList = [
+  "Tarla",
+  "Zara Hatke Zara Bachke",
+  "The Kerala Story",
+  "The Story Of A Beautiful Girl",
+  "Maharashtra Shahir",
+  "Kisi Ka Bhai Kisi Ki Jaan",
+  "Mrs. Undercover ",
+];
+export let slots = ["10:00 AM", "01:00 PM", "03:00 PM", "08:00 PM", "11:30 PM"];
+export let seats = ["A1", "A2", "A3", "A4", "D1", "D2"];
+
+// exports.moviesList = moviesList;
+// exports.slots = slots;
+// exports.seats = seats;
